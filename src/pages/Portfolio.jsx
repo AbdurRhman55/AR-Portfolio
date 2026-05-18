776164090f8099d9096d6f367f735dca57b8e695
Abdur Rahman <203861431+AbdurRhman55@users.noreply.github.com>
2026-05-18 13:07:36 +0500

    upload

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -45,12 +45,12 @@ const Portfolio = ({ onNavigateToHome }) => {
       className="relative w-full h-full bg-[#121212] overflow-x-hidden select-none"
     >
       {/* Hero Section */}
-      <div className="relative w-full h-dvh bg-gradient-to-t from-[#0055FF] via-[#a8b0bd] to-[#121212] flex items-center justify-center overflow-hidden">
+      <div className="relative w-full h-dvh bg-gradient-to-t from-[#0055FF] via-[#a8b0bd] to-[#121212] flex  items-center justify-center overflow-hidden">
         <div className="absolute inset-0 opacity-[0.02] pointer-events-none mix-blend-overlay" style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")` }} />
 
         <Header onHomeClick={onNavigateToHome} onPortfolioClick={() => { }} />
 
-        <div className="relative flex flex-col items-center justify-center w-full">
+        <div className="relative flex flex-col items-center justify-center w-full ">
           <h1
             ref={titleRef}
             className="text-[32vw] md:text-[25vw] font-display text-white/90 leading-none tracking-tighter uppercase select-none"
@@ -86,7 +86,7 @@ const Portfolio = ({ onNavigateToHome }) => {
           </div>
         </div>
 
-        <div className="absolute bottom-12 md:left-1/2 left-0 md:-translate-x-1/2 translate-x-16 text-white/40 text-xs tracking-widest uppercase">
+        <div className="absolute bottom-20 md:bottom-12 left-1/2 -translate-x-1/2 text-white/40 text-[10px] md:text-xs tracking-widest uppercase whitespace-nowrap">
           Explore Excellence • 2026 Edition
         </div>
       </div>
